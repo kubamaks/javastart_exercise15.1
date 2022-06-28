0c39f8e376f575296b451b5638829e718e66a342
kubamak <kubamachnik@gmail.com>
2022-06-28 12:19:42 +0200

    project initial data

---
+++ src/main/java/pl/km/exercise251/Exercise251Application.java
@@ -0,0 +1,13 @@
+package pl.km.exercise251;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise251Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise251Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/km/exercise251/Exercise251ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.km.exercise251;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise251ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
